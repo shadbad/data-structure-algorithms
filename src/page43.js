c7f70c54ab3268e9b91a3ec1650afce74817afe4
Sina Akbari Shadbad <sina.akbarishadbad@appsfactory.de>
2024-01-25 14:13:40 +0100

    review: add suggestions

--- src/page43.js
+++ src/page43.js
@@ -1,7 +1,12 @@
-// Tiny improvement: we can use const instead of let since
-// the "things" variable won't get reassigned
+/**
+ * Tiny improvement:
+ *  we can use const instead of let since the "things" variable won't get reassigned
+ */
 
-let things = ["apples", "baboons", "cribs", "dulcimers"];
+let things = ['apples', 'baboons', 'cribs', 'dulcimers'];
 for (const thing of things) {
   console.log("Here's a thing: " + thing);
 }
+
+// In compliance with AirBnb coding style. But might be a bit hard to read for junior devs
+things.forEach((thing) => console.log(`Here's a thing: ${thing}`));
